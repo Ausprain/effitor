5f373ab73e5a47ceae046e9e43329c96f75780d2
Ausprain <ausprain@qq.com>
2024-04-30 09:31:47 +0800

    docs<mark>

--- src/effitor/plugins/mark/effector/selchange.ts
+++ src/effitor/plugins/mark/effector/selchange.ts
@@ -5,6 +5,7 @@ import { markState } from "../config";
 export const selChange: Et.SelChangeAction = (ev, ctx) => {
     if (markState.phase) {
         // phase == 2 时为刚刚插入标记节点导致的selectionchange
+        // phase == 1 时说明刚刚插入的标记节点没有输入任何内容就又触发了selectionchange, 此时标记节点为空，应当撤销
         if ((markState.phase--) === 1) {
             // console.log('撤销临时节点插入')
             ctx.commandHandler.discard()
