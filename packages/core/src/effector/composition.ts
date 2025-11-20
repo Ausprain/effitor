bd530af7b2e47be410926ff50fc27917b9072089
Ausprain <ausprain@qq.com>
2025-11-20 16:10:42 +0800

    fix<core>: 选区 Range 状态输入法输入后无法撤回问题

--- packages/core/src/effector/composition.ts
+++ packages/core/src/effector/composition.ts
@@ -3,8 +3,13 @@ import type { Et } from '../@types'
 export const getCompositionStart = (ctx: Et.EditorContext) => {
   return () => {
     // 输入法开始时必须强制更新上下文获取最新的光标位置
-    // fixme 删除选区
-    // ctx.forceUpdate()
+    ctx.forceUpdate()
+    // 输入法开始时, 选区非 collapsed, 先删除(自动更新光标位置信息)
+    if (!ctx.selection.isCollapsed) {
+      ctx.commonHandler.removeRangingContents()
+      // 更新光标位置信息, 并滚到可见区域
+      ctx.selection.scrollIntoViewSync()
+    }
     ctx.commandManager.commit()
     ctx.composition.onStart()
   }
