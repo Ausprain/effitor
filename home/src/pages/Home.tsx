cfe47a2014afc8243f3fd34f18dc66cb223431a0
Ausprain <ausprain@qq.com>
2026-01-01 14:38:33 +0800

    fix<home>: 代码块 feature 展示中没有成功退出代码块的问题

--- home/src/pages/Home.tsx
+++ home/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import HomeMainContent from '../components/home-main/TheMainContent'
 
 const Home: React.FC = () => {
   return (
-    <div className="pb-80 mx-auto min-h-screen">
+    <div className="pb-[600px] mx-auto min-h-screen">
       <HomeMainContent />
     </div>
   )
